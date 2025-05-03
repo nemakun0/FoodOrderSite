65cb7f4aed42635d6f5a79d9eae28e18d164def4
Göksu Kesen <gksukesenn@gmail.com>
2025-05-03 23:25:33 +0300

    Restaurant Managing views are created.

---
+++ FoodOrderSite/Controllers/RestaurantSettingsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class RestaurantSettingsController : Controller
+{
+    // GET: /RestaurantSettings
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // POST: /RestaurantSettings
+    [HttpPost]
+    public IActionResult Index(IFormCollection form)
+    {
+        // Burada form verisini alıp işlemler yapabilirsiniz
+        // Ama şu anda sadece görsellik için boş bırakabilirsiniz
+        return View();
+    }
+}
