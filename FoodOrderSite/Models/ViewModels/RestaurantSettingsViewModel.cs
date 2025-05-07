02184f0bb7a3ed2f98362bece2fe1695b1f3f3c4
Göksu Kesen <gksukesenn@gmail.com>
2025-05-07 21:47:22 +0300

    RestaurantSettings sayfası yapıldı.

--- FoodOrderSite/Models/ViewModels/RestaurantSettingsViewModel.cs
+++ FoodOrderSite/Models/ViewModels/RestaurantSettingsViewModel.cs
@@ -1,26 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using FoodOrderSite.Models;
 
 namespace FoodOrderSite.Models.ViewModels
 {
     public class RestaurantSettingsViewModel
     {
-        public int UserId { get; set; }              // Controller kullanıyorsa bu gerekli
-        public int RestaurantId { get; set; }        // Controller kullanıyorsa bu da gerekli
-
+        public int RestaurantId { get; set; }
+        
+        [Required(ErrorMessage = "Restoran adı gereklidir")]
+        [StringLength(100)]
         public string RestaurantName { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public string City { get; set; }
-        public string District { get; set; }
+        
+        [StringLength(50)]
+        public string? RestaurantType { get; set; }
+        
+        [Required(ErrorMessage = "Adres bilgisi gereklidir")]
+        [StringLength(200)]
         public string Address { get; set; }
+        
+        [StringLength(500)]
         public string? Description { get; set; }
-
-        public List<ScheduleDto> Schedules { get; set; }
+        
+        [Required(ErrorMessage = "Şehir bilgisi gereklidir")]
+        [StringLength(50)]
+        public string City { get; set; }
+        
+        [Required(ErrorMessage = "İlçe bilgisi gereklidir")]
+        [StringLength(50)]
+        public string District { get; set; }
+        
+        [Required(ErrorMessage = "Telefon numarası gerekli")]
+        [Phone(ErrorMessage = "Geçersiz telefon numarası")]
+        public string Phone { get; set; }
+        
+        [Required(ErrorMessage = "Email boş olamaz")]
+        [EmailAddress(ErrorMessage = "Geçersiz email adresi")]
+        public string Email { get; set; }
+        
+        public List<ScheduleViewModel> ScheduleItems { get; set; } = new List<ScheduleViewModel>();
     }
-
-    public class ScheduleDto
+    
+    public class ScheduleViewModel
     {
+        public int? ScheduleId { get; set; }
         public string DayOfWeek { get; set; }
         public TimeSpan OpeningTime { get; set; }
         public TimeSpan ClosingTime { get; set; }
